8e542260c170ac20db01e17ec9d88bf5cd3db8aa
Mateusz Piątkowski <mateusz.piatkowski666@gmail.com>
2022-07-02 19:56:54 +0200

    add pie chart with :after background color

---
+++ src/components/CTimer.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="c-timer" @click="onTimerClick">
+    <span class="c-timer__time">{{remainingTime}}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, watch } from "vue";
+const isRunning = ref(false);
+const remainingTime = ref('08:45');
+
+const onTimerClick = () => (isRunning.value = !isRunning.value);
+
+watch(isRunning, () => {
+  if (isRunning.value) {
+    console.log("its running");
+  }
+});
+</script>
+
+<style scoped lang="scss">
+.c-timer {
+  width: 100%;
+  aspect-ratio: 1 / 1;
+  border-radius: 50%;
+  background-color: rgba(#eb2727, 0.6);
+  position: relative;
+
+  &:after {
+    content: '';
+    border-radius: 50%;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(#eb2727, 1);
+  }
+
+  &__time {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 1;
+    font-size: 5rem;
+  }
+}
+</style>
---
+++ src/views/VHome.vue
@@ -0,0 +1,26 @@
+<template>
+  <ion-page>
+    <ion-content class="ion-padding">
+      <c-timer />
+    </ion-content>
+
+    <ion-footer>
+      <ion-toolbar>
+        <ion-title>Footer</ion-title>
+      </ion-toolbar>
+    </ion-footer>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import {
+  IonPage,
+  IonContent,
+  IonFooter,
+  IonToolbar,
+  IonTitle,
+} from "@ionic/vue";
+import CTimer from "@/components/CTimer.vue";
+</script>
+
+<style scoped lang="scss"></style>
