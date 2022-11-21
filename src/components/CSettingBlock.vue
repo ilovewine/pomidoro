a2d4c4a9cf71a02baba723446bac6b6807ae621d
Mateusz Piątkowski <mpiatkowski@sii.pl>
2022-11-21 11:52:33 +0100

    add build.yml, add a setting block with dark mode

---
+++ src/components/CSettingBlock.vue
@@ -0,0 +1,19 @@
+<template>
+  <ion-item>
+    <ion-label>{{ label }}</ion-label>
+    <slot />
+  </ion-item>
+</template>
+
+<script setup lang="ts">
+import { IonLabel, IonItem } from '@ionic/vue';
+
+defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<style scoped lang="scss"></style>
--- src/views/VSettings.vue
+++ src/views/VSettings.vue
@@ -5,15 +5,35 @@
         <ion-title>Settings</ion-title>
       </ion-toolbar>
     </ion-header>
-    
+
     <ion-content>
-      Settings
+      <ion-grid>
+        <ion-row>
+          <ion-col>
+            <ion-list>
+              <c-setting-block label="Dark Mode">
+                <ion-toggle color="primary"></ion-toggle>
+              </c-setting-block>
+            </ion-list>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonList,
+  IonToggle,
+} from '@ionic/vue';
+import CSettingBlock from '@/components/CSettingBlock.vue';
 </script>
-
-<style scoped lang="scss"></style>
