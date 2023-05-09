e60bd15fc7aaea09ddd7330b049fc2e5faa3630d
Mateusz Piątkowski <mpiatkowski@sii.pl>
2023-05-09 19:19:16 +0200

    refactor: :arrow_up: upgrade capacitor

--- src/stores/clock/index.ts
+++ src/stores/clock/index.ts
@@ -5,17 +5,17 @@ import { defineStore } from 'pinia';
 import Clock from './Clock';
 import Time from './Time';
 
-// const DEFAULT_DURATION = {
-//   [clockType.BREAK]: new Time(5, 0, clockType.BREAK),
-//   [clockType.WORK]: new Time(25, 0, clockType.WORK),
-//   [clockType.LONG_BREAK]: new Time(30, 0, clockType.LONG_BREAK),
-// };
-
-const DEFAULT_DURATION: Record<ClockType, Time> = {
-  [ClockType.BREAK]: new Time(0, 5, ClockType.BREAK),
-  [ClockType.WORK]: new Time(0, 4, ClockType.WORK),
-  [ClockType.LONG_BREAK]: new Time(0, 3, ClockType.LONG_BREAK),
+const DEFAULT_DURATION = {
+  [ClockType.BREAK]: new Time(5, 0, ClockType.BREAK),
+  [ClockType.WORK]: new Time(25, 0, ClockType.WORK),
+  [ClockType.LONG_BREAK]: new Time(30, 0, ClockType.LONG_BREAK),
 };
+
+// const DEFAULT_DURATION: Record<ClockType, Time> = {
+//   [ClockType.BREAK]: new Time(0, 5, ClockType.BREAK),
+//   [ClockType.WORK]: new Time(0, 4, ClockType.WORK),
+//   [ClockType.LONG_BREAK]: new Time(0, 3, ClockType.LONG_BREAK),
+// };
 
 const createClockMap = (): Map<ClockType, Clock> => {
   const map = new Map();
