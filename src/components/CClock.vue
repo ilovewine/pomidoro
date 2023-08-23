ea83d502ea232d81e491942cd62902e98f079f71
Mateusz Piątkowski <mateusz.piatkowski666@gmail.com>
2023-08-23 21:18:32 +0200

    feat: :zap: change color of tomatoes, change sound settings

--- src/components/CClock.vue
+++ src/components/CClock.vue
@@ -1,49 +1,26 @@
 <template>
-  <!-- <div class="c-clock">
-    <transition name="fade">
-      <svg viewBox="0 0 40 18" class="c-clock__time">
-        <text x="0" y="15">{{ activeClock.readableTime }}</text>
-      </svg>
-    </transition>
-  </div> -->
-  <!-- <div> -->
   <div class="c-clock">
-    <tomato-icon :primary-fill="'red'" :secondary-fill="'green'" :time="activeClock.readableTime" />
+    <tomato-icon
+      :primary-fill="currentColor.primary"
+      :secondary-fill="currentColor.secondary"
+      :time="activeClock.readableTime" />
   </div>
-  <!-- </div> -->
 </template>
 
 <script setup lang="ts">
 import TomatoIcon from '@/resources/svg/TomatoIcon.vue';
 import useClockStore from '@/stores/clock';
+import useSettingsStore from '@/stores/settings';
 import { computed } from 'vue';
 
-const store = useClockStore();
+const clockStore = useClockStore();
+const settingsStore = useSettingsStore();
 
-const activeClock = computed(() => store.activeClock);
+const activeClock = computed(() => clockStore.activeClock);
+const currentColor = computed(() => settingsStore.currentColor);
 </script>
 
 <style scoped lang="scss">
-// .c-clock {
-//   width: 100%;
-//   max-width: 30rem;
-//   aspect-ratio: 1 / 1;
-//   border-radius: 50%;
-//   background-color: rgba(#eb2727, 1);
-//   position: relative;
-//   margin-bottom: 2rem;
-
-//   &__time {
-//     position: absolute;
-//     left: 50%;
-//     top: 50%;
-//     transform: translate(-50%, -50%);
-//     z-index: 1;
-//     width: 80%;
-//     margin: 0;
-//     fill: var(--ion-color-light);
-//   }
-// }
 .c-clock {
   display: inline-flex;
   justify-content: center;
--- src/stores/settings/index.ts
+++ src/stores/settings/index.ts
@@ -1,6 +1,8 @@
 import SettingsStore from '@/types/settings/SettingsStore.interface';
 import { defineStore } from 'pinia';
 import defaultBeep from '@/resources/audio/defaultBeep.wav';
+import { ClockType } from '@/types/clock/ClockType';
+import useClockStore from '@/stores/clock';
 
 const audio = new Audio(defaultBeep);
 
@@ -8,13 +10,33 @@ const useSettingsStore = defineStore('settings', {
   state: (): SettingsStore => ({
     isDarkModeOn: false,
     isMillisecondsOn: false,
-    isSoundsOn: false,
-    defaultSound: new Audio(defaultBeep),
+    isSoundsOn: true,
+    sound: audio,
+    colors: {
+      [ClockType.BREAK]: {
+        primary: '#e0c111',
+        secondary: '#1d6113',
+      },
+      [ClockType.WORK]: {
+        primary: '#b90505',
+        secondary: '#1d6113',
+      },
+      [ClockType.LONG_BREAK]: {
+        primary: '#380808',
+        secondary: '#1d6113',
+      },
+    },
   }),
+  getters: {
+    currentColor: state => {
+      const clockStore = useClockStore();
+      return state.colors[clockStore.activeClockType];
+    },
+  },
   actions: {
     playSound() {
       if (this.isSoundsOn) {
-        audio.play();
+        this.sound.play();
       }
     },
   },
